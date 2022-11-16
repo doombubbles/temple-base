a45ac322f309018dfa10dc14987670ffa450e157
doombubbles <obsidiantoolzg@gmail.com>
2022-11-16 10:37:22 -0800

    Initial commit

---
+++ ModHelperData.cs
@@ -0,0 +1,12 @@
+namespace TempleBase;
+
+public static class ModHelperData
+{
+    public const string Version = "1.0.0";
+    public const string Name = "TempleBase";
+
+    public const string Description = "An empty mod";
+
+    public const string RepoOwner = "doombubbles";
+    public const string RepoName = "temple-base";
+}
---
+++ TempleBaseMod.cs
@@ -0,0 +1,12 @@
+using MelonLoader;
+using BTD_Mod_Helper;
+using TempleBase;
+
+[assembly: MelonInfo(typeof(TempleBaseMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace TempleBase;
+
+public class TempleBaseMod : BloonsTD6Mod
+{
+}
